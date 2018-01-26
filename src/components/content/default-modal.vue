5b8f1e66e36d6ed899e01d0d84b08750898657d0
n_korolev <nk@imagespark.ru>
2018-01-26 20:36:01 +0300

    Add lodash, formatted code

--- src/components/content/default-modal.vue
+++ src/components/content/default-modal.vue
@@ -1,207 +1,232 @@
 <template>
-	<section class="modal">
-		<transition :name="animName">
-			<section class="modal__window" :style="styleObj" v-show="showModal">
-		        <div class="modal__header">
-		            <slot name='modal-header'></slot>
-		        </div>
-		        <div class="modal__content">
-		            <slot name='modal-content'></slot>
-		        </div>
-		        <div class="modal__footer">
-		        	<slot name='modal-footer'></slot>
-		        </div>
-			</section>
-		</transition>
-		<transition name="fade">
-			<div class="modal__background" v-show="showModal" @click="closeModal"></div>
-		</transition>
-	</section>
-</template>
-<script>
-	export default {
-	    name: "modal",
-	    methods: {
-	        closeModal(){
-	            this.$emit('closeModal');
-	        },
-	    },
-	    computed:{
-	    	styleObj(){
-	    		return {
-	    			'width' : this.width + "px",
-	    			'height' : this.height + "px",
-	    		}
-	    	}
-	    },
-	    props:{
-	    	animName:{
-	    		type: [String],
-			    default: function () {
-			       return "fade"
-			    }
-	    	},
-	    	showModal:{
-	    		type: [Boolean],
-			    default: function () {
-			       return false
-			    }
-	    	},
-	    	width: {
-        		type: String,
-                validator: function(value) {
-                    return (value !== null)
-                },
-			    default: function () {
-			       return ""
-			    }
-        	},
-        	height: {
-        		type: String,
-                validator: function(value) {
-                    return (value !== null)
-                },
-			    default: function () {
-			       return ""
-			    }
-        	},
-	    }
-	}
-</script>
-<style lang="scss">
-	.fade-enter-active {
-		transition: opacity .5s
-	}
-	.fade-leave-active {
-	  transition: opacity 1s
-	}
-	.fade-enter, .fade-leave-active{
-	  opacity: 0
-	}
-	.bounce-enter-active {
-	  animation: bounce-in .5s;
-	}
-	.bounce-leave-active {
-	  animation: bounce-out .5s;
-	}
-	.hinge-enter-active {
-	   animation: bounce-in .5s;
-	}
-	.hinge-leave-active {
-	  animation: hinge-out 1s;
-	}
-	.modal{
+<section class="modal">
+    <transition :name="animName">
+        <section class="modal__window"
+                 :style="styleObj"
+                 v-show="showModal">
+            <div class="modal__header">
+                <slot name='modal-header'></slot>
+            </div>
+            <div class="modal__content">
+                <slot name='modal-content'></slot>
+            </div>
+            <div class="modal__footer">
+                <slot name='modal-footer'></slot>
+            </div>
+        </section>
+    </transition>
+    <transition name="fade">
+        <div class="modal__background"
+             v-show="showModal"
+             @click="closeModal"></div>
+    </transition>
+</section>
 
-		&__header, &__content, &__footer{
-			margin-bottom: 30px;
-			overflow: hidden;
-		}
-		&__content{
-			word-wrap: break-word;
-    		max-height: 50%;
-		}
-		&__window{
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			z-index: 1;
-			box-shadow: 0 0 10px 0;
-			transform: translate(-50%, -50%);
-			text-align: center;
-			background-color: #fff;
-		    padding: 20px;
-		    box-sizing: border-box;
-			@keyframes hinge-out {
-				0% { 
-					transform: translate(-50%, -50%) rotate(0); 
-					transform-origin: top left; 
-					animation-timing-function: ease-in-out; 
-				}	
-				20%, 60% { 
-					transform: translate(-50%, -50%) rotate(80deg); 
-					transform-origin: top left; 
-					animation-timing-function: ease-in-out; 
-				}	
-				40% { 
-					transform: translate(-50%, -50%) rotate(60deg); 
-					transform-origin: top left; 
-					animation-timing-function: ease-in-out; 
-				}	
-				80% { 
-					transform: translate(-50%, -50%) rotate(60deg) translateY(0); 
-					opacity: 1; transform-origin: top left; 
-					animation-timing-function: ease-in-out; 
-				}	
-				100% { 
-					transform: translate(-50%, -50%) translateY(700px); 
-					opacity: 0; 
-				}
-			}
-			@keyframes bounce-in {
-				  0% {
-				    transform: translate(-50%, -50%) scale(0);
-				  }
-				  50% {
-				    transform: translate(-50%, -50%) scale(1.5);
-				  }
-				  100% {
-				    transform: translate(-50%, -50%) scale(1);
-				  }
-			}
-			@keyframes bounce-out {
-				  0% {
-				    transform: translate(-50%, -50%) scale(1);
-				  }
-				  50% {
-				    transform: translate(-50%, -50%) scale(1.5);
-				  }
-				  100% {
-				    transform: translate(-50%, -50%) scale(0);
-				  }
-			}
-		}
-		&__background{
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
-			background-color: rgba(81, 100, 107, 0.7);
-			z-index: 0;
-		}
-		&__exit{
-			outline: none;
-			border: none;
-    		background: none;
-			position: absolute;
-	    	top: 15px;
-		    right: 15px;
-		    width: 30px;
-		    height: 30px;
-			overflow: hidden;
-			cursor: pointer;
-			&:hover {
-			    &::before, &::after {
-			      background: #1ebcc5;
-			    }
-			}
-			&::before, &::after {
-			    content: '';
-			    position: absolute;
-			    height: 2px;
-			    width: 100%;
-			    top: 50%;
-			    left: 0;
-			    margin-top: -1px;
-			    background: #000;
-			    border-radius: 80% 0;
-			 }
-			&::before {
-			    transform: rotate(45deg);
-			  }
-			&::after {
-			    transform: rotate(-45deg);
-			}
-		}
-	}
+</template>
+
+<script>
+export default {
+    name: 'modal',
+    methods: {
+        closeModal() {
+            this.$emit('closeModal');
+        }
+    },
+    computed: {
+        styleObj() {
+            return {
+                'width': this.width + 'px',
+                'height': this.height + 'px'
+            };
+        }
+    },
+    props: {
+        animName: {
+            type: [
+                String
+            ],
+            default: function() {
+                return 'fade';
+            }
+        },
+        showModal: {
+            type: [
+                Boolean
+            ],
+            default: function() {
+                return false;
+            }
+        },
+        width: {
+            type: String,
+            validator: function(value) {
+                return (value !== null);
+            },
+            default: function() {
+                return '';
+            }
+        },
+        height: {
+            type: String,
+            validator: function(value) {
+                return (value !== null);
+            },
+            default: function() {
+                return '';
+            }
+        }
+    }
+}
+
+</script>
+
+<style lang="scss">
+.fade-enter-active {
+    transition: opacity .5s
+}
+
+.fade-leave-active {
+    transition: opacity 1s
+}
+
+.fade-enter,
+.fade-leave-active {
+    opacity: 0
+}
+
+.bounce-enter-active {
+    animation: bounce-in .5s;
+}
+
+.bounce-leave-active {
+    animation: bounce-out .5s;
+}
+
+.hinge-enter-active {
+    animation: bounce-in .5s;
+}
+
+.hinge-leave-active {
+    animation: hinge-out 1s;
+}
+
+.modal {
+    &__header,
+    &__content,
+    &__footer {
+        margin-bottom: 30px;
+        overflow: hidden;
+    }
+    &__content {
+        word-wrap: break-word;
+        max-height: 50%;
+    }
+    &__window {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        z-index: 1;
+        box-shadow: 0 0 10px 0;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        background-color: #fff;
+        padding: 20px;
+        box-sizing: border-box;
+        @keyframes hinge-out {
+            0% {
+                transform: translate(-50%, -50%) rotate(0);
+                transform-origin: top left;
+                animation-timing-function: ease-in-out;
+            }
+            20%,
+            60% {
+                transform: translate(-50%, -50%) rotate(80deg);
+                transform-origin: top left;
+                animation-timing-function: ease-in-out;
+            }
+            40% {
+                transform: translate(-50%, -50%) rotate(60deg);
+                transform-origin: top left;
+                animation-timing-function: ease-in-out;
+            }
+            80% {
+                transform: translate(-50%, -50%) rotate(60deg) translateY(0);
+                opacity: 1;
+                transform-origin: top left;
+                animation-timing-function: ease-in-out;
+            }
+            100% {
+                transform: translate(-50%, -50%) translateY(700px);
+                opacity: 0;
+            }
+        }
+        @keyframes bounce-in {
+            0% {
+                transform: translate(-50%, -50%) scale(0);
+            }
+            50% {
+                transform: translate(-50%, -50%) scale(1.5);
+            }
+            100% {
+                transform: translate(-50%, -50%) scale(1);
+            }
+        }
+        @keyframes bounce-out {
+            0% {
+                transform: translate(-50%, -50%) scale(1);
+            }
+            50% {
+                transform: translate(-50%, -50%) scale(1.5);
+            }
+            100% {
+                transform: translate(-50%, -50%) scale(0);
+            }
+        }
+    }
+    &__background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(81, 100, 107, 0.7);
+        z-index: 0;
+    }
+    &__exit {
+        outline: none;
+        border: none;
+        background: none;
+        position: absolute;
+        top: 15px;
+        right: 15px;
+        width: 30px;
+        height: 30px;
+        overflow: hidden;
+        cursor: pointer;
+        &:hover {
+            &::before,
+            &::after {
+                background: #1ebcc5;
+            }
+        }
+        &::before,
+        &::after {
+            content: '';
+            position: absolute;
+            height: 2px;
+            width: 100%;
+            top: 50%;
+            left: 0;
+            margin-top: -1px;
+            background: #000;
+            border-radius: 80% 0;
+        }
+        &::before {
+            transform: rotate(45deg);
+        }
+        &::after {
+            transform: rotate(-45deg);
+        }
+    }
+}
 </style>
--- src/components/content/slider.vue
+++ src/components/content/slider.vue
@@ -1,164 +1,164 @@
 <template>
-	<section class="slider">
-			<div class="slider__inner">
-            	<div class="slider__wrapper" :style="{'transform': 'translateX(' + sliderDiff +'px)'}" ref="slider">
-            	
-	        			<div class="slider__item"  v-for="slide in colLen" >
-		                    <ul>
-		                       <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, sequi nobis. Facere iste quis maxime tenetur repudiandae. Eos, saepe, culpa. Nihil amet ad, sint voluptas corporis consequatur odit vitae! Inventore ullam fugit ipsum qui temporibus accusantium nihil repudiandae quo, voluptatem dolores ab, dolorum quia doloremque quasi magnam, dolore maiores impedit.</li>
-		                       <li>Aliquid harum nobis dolore non odit id cumque voluptatem itaque optio molestiae esse blanditiis eos, consectetur vero tenetur ut maiores alias corporis nemo nesciunt omnis molestias reprehenderit doloribus repellat. Voluptate hic dignissimos, nam error, ipsa commodi molestias ad accusantium perferendis, similique ut consequuntur dolores obcaecati distinctio ipsam! Ipsum, asperiores, tenetur.</li>
-		                     
-		                    </ul>
-	               		</div>
-            	</div>
-			</div>
-		<div class="slider__navigation">
-			<span 
-				class="slider__navigation-point" 
-				v-for="(e, i) in colLen" 
-				@click="setSlide(i)" 
-				:class="{
-					'slider__navigation-point--active': i === current
-				}">
-			</span>
-		</div>
-		<button 
-			class="slider__button slider__button--prev"
-			@click="goPrev"
-			:class="{
-				'slider__button--disabled': current === 0
-			}">&lt;
-		</button>
-		<button 
-			class="slider__button slider__button--next" 
-			@click="goNext"
-			:class="{
-				'slider__button--disabled': current === colLen-1
-			}">&gt;
-		</button>
-	</section>
-</template>
-<script>
-	export default {
-		name: "slider",
-		data(){
-			return{
-				colLen: 3,
-				current: 0,
-				sliderWidth: 0
-			}
-		},
-		mounted(){
-			this.sliderWidth = this.$refs.slider.clientWidth;
-		},
-		computed: {
-			sliderDiff() {
-				return - this.sliderWidth * this.current
-			}
-		},
-		methods: {
-			setSlide(id){
-              if(id < this.colLen && id >= 0){
-                 this.current = id;
-                 this.setActiveElem(this.current);
-                }
-            },
-			goNext(){
-              if(this.current+1 < this.colLen){
-                 this.current++;
-                 this.setActiveElem(this.current);
-                }
-            },
-            goPrev(){
-              if(this.current - 1 >= 0){
-                 this.current--;
-                 this.setActiveElem(this.current);
-                } 
+<section class="slider">
+    <div class="slider__inner">
+        <div class="slider__wrapper"
+             :style="{'transform': 'translateX(' + sliderDiff +'px)'}"
+             ref="slider">
+            <div class="slider__item"
+                 v-for="slide in colLen">
+                <ul>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, sequi nobis. Facere iste quis maxime tenetur repudiandae. Eos, saepe, culpa. Nihil amet ad, sint voluptas corporis consequatur odit vitae! Inventore ullam fugit ipsum qui temporibus accusantium nihil repudiandae quo, voluptatem dolores ab, dolorum quia doloremque quasi magnam, dolore maiores impedit.</li>
+                    <li>Aliquid harum nobis dolore non odit id cumque voluptatem itaque optio molestiae esse blanditiis eos, consectetur vero tenetur ut maiores alias corporis nemo nesciunt omnis molestias reprehenderit doloribus repellat. Voluptate hic dignissimos, nam error, ipsa commodi molestias ad accusantium perferendis, similique ut consequuntur dolores obcaecati distinctio ipsam! Ipsum, asperiores, tenetur.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="slider__navigation">
+        <span class="slider__navigation-point"
+              v-for="(e, i) in colLen"
+              @click="setSlide(i)"
+              :class="{
+                    'slider__navigation-point--active': i === current
+                }">
+            </span>
+    </div>
+    <button class="slider__button slider__button--prev"
+            @click="goPrev"
+            :class="{
+                'slider__button--disabled': current === 0
+            }">&lt;
+    </button>
+    <button class="slider__button slider__button--next"
+            @click="goNext"
+            :class="{
+                'slider__button--disabled': current === colLen-1
+            }">&gt;
+    </button>
+</section>
 
-            },
-            setActiveElem(id){
-               
-            },
-		}
-	}
+</template>
+
+<script>
+export default {
+    name: 'slider',
+    data() {
+        return {
+            colLen: 3,
+            current: 0,
+            sliderWidth: 0
+        };
+    },
+    mounted() {
+        this.sliderWidth = this.$refs.slider.clientWidth;
+    },
+    computed: {
+        sliderDiff() {
+            return -this.sliderWidth * this.current;
+        }
+    },
+    methods: {
+        setSlide(id) {
+            if (id < this.colLen && id >= 0) {
+                this.current = id;
+                this.setActiveElem(this.current);
+            }
+        },
+        goNext() {
+            if (this.current + 1 < this.colLen) {
+                this.current++;
+                this.setActiveElem(this.current);
+            }
+        },
+        goPrev() {
+            if (this.current - 1 >= 0) {
+                this.current--;
+                this.setActiveElem(this.current);
+            }
+
+        },
+        setActiveElem(id) {}
+    }
+}
+
 </script>
+
 <style lang="scss">
-	.slider{
-		background-color: #fff;
-		box-shadow: 0 0 10px 0;
-		overflow: hidden;
-	    padding: 20px;
-	    box-sizing: border-box;
-	    max-width: 800px;
-   		min-width: 530px;
-   		margin: 0 auto;
-   		position: relative;
-   		li{
-   			list-style-position: inside;
-   		}
-   		&-scroll{
-   			width: 100%;
-    		height: 100%;
-   		}
-   		&__inner{
-			width: calc(100% - 100px);
-			margin-left: auto;
-    		margin-right: auto;
-   			height: 350px;
-   			overflow: hidden;
-   		}
-   		&__wrapper{
-   			white-space: nowrap;
-   			transition: .3s;
-   		}
-   		&__button{
-			border: none;
-		    font-size: 36px;
-		    background-color: #fff;
-		    cursor: pointer;
-		    outline: none;
-		    &--disabled{
-		    	opacity: .3;
-		    	cursor: auto;
-		    }
-			&--prev{
-				position: absolute;
-				top: 50%;
-				transform: translateY(-50%);
-				left: 20px;
-			}
-			&--next{
-				position: absolute;
-				top: 50%;
-				transform: translateY(-50%);
-				right: 20px;
-			}
-   		}
-   		&__navigation{
-			text-align: center;
-			margin-top: 15px;
-   			&-point{
-			    width: 15px;
-			    height: 15px;
-			    background-color: #716e6e;
-			    opacity: .3;
-			    border-radius: 50%;
-			    border: 1px solid #000;
-			    margin-right: 20px;
-			    display: inline-block;
-			    box-sizing: border-box;
-			    cursor: pointer;
-			    vertical-align: bottom;	
-			    &--active{
-			    	opacity: 1;
-			    }
-   			}
-   		}
-   		&__item{
-			display: inline-block;
-   			ul {
-   				white-space: normal;
-   			}
-   		}
-	}
+.slider {
+    background-color: #fff;
+    box-shadow: 0 0 10px 0;
+    overflow: hidden;
+    padding: 20px;
+    box-sizing: border-box;
+    max-width: 800px;
+    min-width: 530px;
+    margin: 0 auto;
+    position: relative;
+    li {
+        list-style-position: inside;
+    }
+    &-scroll {
+        width: 100%;
+        height: 100%;
+    }
+    &__inner {
+        width: calc(100% - 100px);
+        margin-left: auto;
+        margin-right: auto;
+        height: 350px;
+        overflow: hidden;
+    }
+    &__wrapper {
+        white-space: nowrap;
+        transition: .3s;
+    }
+    &__button {
+        border: none;
+        font-size: 36px;
+        background-color: #fff;
+        cursor: pointer;
+        outline: none;
+        &--disabled {
+            opacity: .3;
+            cursor: auto;
+        }
+        &--prev {
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            left: 20px;
+        }
+        &--next {
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            right: 20px;
+        }
+    }
+    &__navigation {
+        text-align: center;
+        margin-top: 15px;
+        &-point {
+            width: 15px;
+            height: 15px;
+            background-color: #716e6e;
+            opacity: .3;
+            border-radius: 50%;
+            border: 1px solid #000;
+            margin-right: 20px;
+            display: inline-block;
+            box-sizing: border-box;
+            cursor: pointer;
+            vertical-align: bottom;
+            &--active {
+                opacity: 1;
+            }
+        }
+    }
+    &__item {
+        display: inline-block;
+        ul {
+            white-space: normal;
+        }
+    }
+}
 </style>
